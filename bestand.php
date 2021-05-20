133535d17f9bf2233e4007ab2fb016142c9db7de
stanBrusse <60739397+stanBrusse@users.noreply.github.com>
2021-05-20 13:49:18 +0200

    Update bestand.php

--- bestand.php
+++ bestand.php
@@ -1,10 +1,11 @@
 <?php
-$conn = mysqli_connect("mysql://sbrusse.gc-webhosting.nl:3306", "sbrusse", "Heelweg1!", "sbrusse_jokeapp");
+$conn = mysqli_connect("gc-webhosting.nl",
+ "sbrusse_jokeapp", "sbrusse_jokeapp", "sbrusse_jokeapp");
 $sql = "SELECT * FROM jokes ORDER BY id";
 $result = mysqli_query($conn, $sql);
 while ($row = mysqli_fetch_array($result)) {
-echo $row["id"];
+echo $row["id"] . " " . $row["title"] . "<br>";
 }
 
-echo "dit  echt goedd";
+
 ?>
